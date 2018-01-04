80944b1bfa69a3094d40bb6bfbb9da7dbe14fc7b
yangchuanhu <ych_1024@163.com>
2018-01-04 17:15:40 +0800

    增加四舍五入方法

--- mathx/common.go
+++ mathx/common.go
@@ -1,5 +1,9 @@
 package mathx
 
+import (
+    "math"
+)
+
 // 计算某个值的平方根结果
 func Sqrt(x float64) float64 {
 	z := 0.0
@@ -7,4 +11,33 @@ func Sqrt(x float64) float64 {
 		z -= (z*z - x) / (2 * x)
 	}
 	return z
+}
+
+// 四舍五入
+func Round(val float64, places int) float64 {
+    var t float64
+    f := math.Pow10(places)
+    x := val * f
+    if math.IsInf(x, 0) || math.IsNaN(x) {
+        return val
+    }
+    if x >= 0.0 {
+        t = math.Ceil(x)
+        if (t - x) > 0.50000000001 {
+            t -= 1.0
+        }
+    } else {
+        t = math.Ceil(-x)
+        if (t + x) > 0.50000000001 {
+            t -= 1.0
+        }
+        t = -t
+    }
+    x = t / f
+
+    if !math.IsInf(x, 0) {
+        return x
+    }
+
+    return t
 }
