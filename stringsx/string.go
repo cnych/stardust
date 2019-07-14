19d251e088601cabcdadd907924612d8377bc9ca
yangchuanhu <ych_1024@163.com>
2019-07-14 17:54:33 +0800

    change Hello function to support lang

--- stringsx/string.go
+++ stringsx/string.go
@@ -55,6 +55,15 @@ func CheckPhone(s string) bool {
 	return true
 }
 
-func Hello(name string) string {
-	return fmt.Sprintf("Hi, %s", name)
+func Hello(name, lang string) (string, error) {
+	switch lang {
+	case "en":
+		return fmt.Sprintf("Hi, %s!", name), nil
+	case "zh":
+		return fmt.Sprintf("你好, %s!", name), nil
+	case "fr":
+		return fmt.Sprintf("Bonjour, %s!", name), nil
+	default:
+		return "", fmt.Errorf("unknow language")
+	}
 }
